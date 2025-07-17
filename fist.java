a7e2a9c6095fa0f9c8fe5132e66cd4846c99c087
Madhan Raj R <rmadhanraj540@gmail.com>
2025-07-17 10:00:36 +0530

    for day 17

---
+++ fist.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class fist {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        int arr[] = new int[size];
+        for(int i=0;i<size;i++){
+            arr[i] = scan.nextInt();
+        }
+        List<Integer> al = new ArrayList<Integer>();
+        for(int i=1;i<size-1;i++){
+            if(arr[i]>arr[i-1]&&arr[i]>arr[i+1]){
+                al.add(arr[i]);
+            }
+        }
+        System.out.print(al);
+    }
+}
